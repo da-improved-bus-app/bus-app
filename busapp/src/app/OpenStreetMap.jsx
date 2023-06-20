be3aca8bf6f3d8e3dfcce9bcdc087707cd3a8a45
Giorgio <tran.giorgio@gmail.com>
2023-06-19 21:33:50 -1000

    Add functioning map

---
+++ busapp/src/app/OpenStreetMap.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useRef } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const OpenStreetMap = () => {
+  const [center, setCenter] = useState({ lat: -4.043477, lng: 39.668205 });
+  const ZOOM_LEVEL = 9;
+  const mapRef = useRef();
+
+  return (
+    <>
+      <h1>OpenStreetMap Embeded</h1>
+      <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef} className="h-96">
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {location.loaded && !location.error && (
+          <Marker
+            position={[location.coordinates.lat, location.coordinates.lng]}
+          ></Marker>
+        )}
+      </MapContainer>
+    </>
+  );
+};
+
+export default OpenStreetMap;
